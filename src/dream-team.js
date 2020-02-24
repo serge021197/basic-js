062dcefdf07ead31c90d0eedb0f74847a72a45b7
serge021197 <sergey-v-1997@mail.ru>
2020-02-24 14:57:58 +0400

    fix: fix some bags

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,13 +1,10 @@
 module.exports = function createDreamTeam(members) {
   let teamArray = [];
-
   if (!Array.isArray(members)) return false;
-
   members.forEach(elem => {
     if (typeof elem === 'string') {
-      teamArray.push(elem[0].toUpperCase());
+      teamArray.push(elem.trim()[0].toUpperCase());
     }
   });
-
   return teamArray.sort().join('');
 };
