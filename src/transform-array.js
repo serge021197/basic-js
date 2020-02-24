0db509654f00ba2a802ac59a6d080dcae9254efb
serge021197 <sergey-v-1997@mail.ru>
2020-02-24 21:02:22 +0400

    feat: add solution for task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,16 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+    if (!Array.isArray(arr)) throw new Error;
+    let transformArray = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === '--discard-prev') {
+            transformArray.pop();
+        } else if (arr[i] === '--discard-next') {
+            if (i < arr.length - 1) i++;
+        } else if (arr[i] === '--double-next') {
+            if (i < arr.length - 1) transformArray.push(arr[i + 1]);
+        } else if (arr[i] === '--double-prev') {
+            if (i > 0) transformArray.push(arr[i - 1]);
+        } else transformArray.push(arr[i])
+    }
+    return transformArray;
 };
